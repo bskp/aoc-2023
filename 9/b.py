46b03140b3b8c089aa26cb9ce80bdb2dc6c0cb9f
Matthias Roggo <matthias.roggo@gmail.com>
2023-12-09 09:42:05 +0100

    9

---
+++ 9/b.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+def extrapolate(values: [int]):
+    diff = [values[i + 1] - values[i] for i in range(len(values) - 1)]
+    if all([d == 0 for d in diff]):
+        return values[0]
+    return values[0] - extrapolate(diff)
+
+sum = 0
+with open('input.txt', 'r') as f:
+    for row in f:
+        values = [int(v) for v in row.split(' ')]
+        extra = extrapolate(values)
+        print(extra)
+        sum += extra
+
+print('Sum', sum)
